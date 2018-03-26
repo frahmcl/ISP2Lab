66db67225ad4433c0c99dc3745b08b44a67c75cc
Chris Frahm <frahmcl@gmail.com>
2018-03-26 13:19:06 -0500

    Lab Completed

--- ISPExample/src/isp/lab1/Animal.java
+++ ISPExample/src/isp/lab1/Animal.java
@@ -2,7 +2,7 @@ package src.isp.lab1;
 //split interface workable into two interfaces.  Car did not use any of these methods
 //so it is unnecessary for it to implement them.
 
-public interface Animal {
+frahpublic interface Animal {
     public void talk(String s);
     public void run(int distance);
     public void jump();
